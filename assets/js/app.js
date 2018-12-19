9d5d7855caccd6a7e4e9b05a639036b35b5234b1
Chris McCord <chris@chrismccord.com>
2018-12-19 14:26:55 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,32 +2,7 @@ import css from "../css/app.css";
 import "phoenix_html"
 import LiveSocket from "./live_view"
 
-// window.addEventListener("keydown", e => {
-//   if(e.which === 32){ e.preventDefault() }
-// })
-
-let hooks = {}
-
-window.Email = {
-  upcase(e){
-    window.e = e
-    console.log("app", e)
-    e.stopImmediatePropagation()
-    e.target.value = e.target.value.toUpperCase()
-    console.log("stopping", e.target.value)
-  }
-}
-hooks.Phone = {
-  format(input){
-    let numbers = input.value.replace(/\D/g, "").split("")
-    let char = {0: "", 3: "-", 6: "-"}
-    input.value = ""
-    numbers.forEach((num, i) => input.value += `${char[i] || ""}${num}`)
-    return true
-  }
-}
-
-let liveSocket = new LiveSocket("/live", {hooks: hooks})
+let liveSocket = new LiveSocket("/live")
 liveSocket.connect()
 
 
