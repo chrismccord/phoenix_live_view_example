1dba6db8987a797262f5a473de4871219f99a19e
Jørgen Orehøj Erichsen <joe@erichsen.net>
2019-03-17 08:23:12 +0100

    First working version

---
+++ lib/demo_web/live/top_live.ex
@@ -0,0 +1,32 @@
+defmodule DemoWeb.TopLive do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~L"""
+    <pre>
+      <%= @top %>
+    </pre>
+    """
+  end
+
+  def mount(_session, socket) do
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+
+    {:ok, put_top(socket)}
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, put_top(socket)}
+  end
+
+  defp put_top(socket) do
+    case :os.type() do
+      {:unix, :darwin} ->
+        {top, 0} = System.cmd("top", ["-l", "1"])
+        assign(socket, top: top)
+      {:unix, :linux} ->
+        {top, 0} = System.cmd("top", ["-n", "1", "-b"])
+        assign(socket, top: top)
+    end
+  end
+end
