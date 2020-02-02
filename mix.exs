d6ccafc82938c83665d955afdec36b463adbfc00
José Valim <jose.valim@dashbit.co>
2020-02-02 18:49:03 +0100

    Rollback to earlier Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Demo.Mixfile do
     [
       app: :demo,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
