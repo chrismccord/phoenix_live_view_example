3956f0f84e68c5e493535bd49fbb7bad9bc75fc5
José Valim <jose.valim@dashbit.co>
2020-03-02 14:45:46 +0100

    Update to latest live view

--- lib/demo_web/router.ex
+++ lib/demo_web/router.ex
@@ -5,7 +5,7 @@ defmodule DemoWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug Phoenix.LiveView.Flash
+    plug :fetch_live_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :put_layout, {DemoWeb.LayoutView, :app}
--- mix.exs
+++ mix.exs
@@ -33,8 +33,7 @@ defmodule Demo.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.9"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:phoenix_live_view, "~> 0.8.0"},
       {:plug_cowboy, "~> 2.0"},
       {:plug, "~> 1.8"},
       {:phoenix_pubsub, "~> 1.1"},
