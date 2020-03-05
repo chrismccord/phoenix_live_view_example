e3966aca18f7d8f4da84d197e3ee22af4050fd5e
José Valim <jose.valim@dashbit.co>
2020-03-05 19:51:52 +0100

    Initial cookie support to request logger

--- lib/demo_web/endpoint.ex
+++ lib/demo_web/endpoint.ex
@@ -7,8 +7,7 @@ defmodule DemoWeb.Endpoint do
     signing_salt: "3YGvy8FF"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   socket "/socket", DemoWeb.UserSocket,
     websocket: true,
@@ -32,7 +31,10 @@ defmodule DemoWeb.Endpoint do
     plug Phoenix.CodeReloader
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger, param_key: "request_logger"
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
