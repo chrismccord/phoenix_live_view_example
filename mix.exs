0c0926c11a9d0f6a43ecfe0da2d9ff5883ebd0c1
Martin Dluhoš <dluhos.martin@gmail.com>
2023-01-10 10:17:47 +0100

    chore: update elixir version and mix dependencies

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Demo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
